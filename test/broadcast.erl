61a06e29367c62356e829d4fad9a55b11b0a685b
aerl-laquila <dn.tan7388@gmail.com>
2016-05-30 16:51:52 +0200

    Update broadcast.erl

--- test/broadcast.erl
+++ test/broadcast.erl
@@ -26,23 +26,8 @@ process1() ->
     Env = #{group => a, role => helper, battery => 50},
     aerlang:register(Env),
     Pr = "group = c and role =/= this.role",
-    Variable = 10,
-    Pi = aerlang:eval_pred(Pr,Env),
-    F = fun(Foo) ->
-	receive
-	    {Ps, Msg, V} ->
-		case aerlang:check_pred(Ps, Env) andalso aerlang:check_pred(Pi,V) of
-		    true ->
-			    %V1 = [Y || {_,Y} <- V],
-			    self() ! {Msg, V},
-			    receive
-				{Msg, V2} ->
-				    io:format("receive ~p with value ~p ~n", [Msg,V2]);
-				    %% Works with recursively call process1();
-				done -> ok
-			    end;
-		    false -> Foo(Foo)
-		end
-	end
-	end,
-    F(F).
+    from(Pr),
+    receive
+    	{Msg,V} ->
+    		io:format("Receive Message ~p with values ~p~n",[Msg,V])
+    end.
